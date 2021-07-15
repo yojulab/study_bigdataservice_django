a4f62a92f2c87376302d5f66732667da49dc966f
webdessin <webdessin@naver.com>
2021-07-15 14:12:40 +0900

    third commit

--- test_12.py
+++ test_12.py
@@ -3,4 +3,7 @@ import bs4
 import requests
 
 # 추가 입력됨. B: www.google.com, A: www.naver.com
-requests.get('www.naver.com')   # B
+res = requests.get('www.naver.com')   # A
+
+if res.status_code == 300:  # A
+    pass
