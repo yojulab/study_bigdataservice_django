cd0079c4aeda073de42aa378aad79bcaca601519
scw <kaya1436@daum.net>
2021-07-15 14:12:22 +0900

    third commit

--- test_04.py
+++ test_04.py
@@ -2,5 +2,8 @@ import bs4
 
 import requests
 
-# 추가 입력됨. B : google.com, A : www.naver.com
-requests.get('www.naver.com') # A
+# 추가 입력됨.
+res = requests.get('www.google.com')
+
+if res.status_code == 200:
+    pass
