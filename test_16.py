336889b589aae48e064d778b07dc06f1e005046a
user <yoonputer@naver.com>
2021-07-15 14:12:30 +0900

    third commit A

--- test_16.py
+++ test_16.py
@@ -3,4 +3,7 @@ import bs4
 import requests
 
 # 추가 입력됨. B : google.com, A: www.naver.com
-requests.get('www.google.com') # B
+res = requests.get('www.google.com') # B
+
+if res.status_code == 300:  #A
+    pass
