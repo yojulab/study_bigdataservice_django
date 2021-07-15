de1bc8f38bf95cb301a11ee5e89c218ef14aa0ba
doka <posmane5@gmail.com>
2021-07-15 14:25:31 +0900

    afsd

--- test_16.py
+++ test_16.py
@@ -3,7 +3,4 @@ import bs4
 import requests
 
 # 추가 입력됨. B : google.com, A: www.naver.com
-res = requests.get('www.google.com') # B
-
-if res.status_code == 300:  #A
-    pass
+requests.get('www.google.com')  # B
