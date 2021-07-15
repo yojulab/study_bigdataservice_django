dd8d209f6f0eaeaf6a153f8b07c28864e76713b0
SanghunOh <otter.oh@gmail.com>
2021-07-15 14:12:26 +0900

    thred commit

--- test_16.py
+++ test_16.py
@@ -3,4 +3,8 @@ import bs4
 import requests
 
 # 추가 입력됨. B : google.com, A: www.naver.com
-requests.get('www.google.com') # B
+res = requests.get('www.google.com') # B
+
+if res.status_code == 200:  #B
+    pass
+
