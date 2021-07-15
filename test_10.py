ddae11adc9776721f1bb9e4b30faaf9ecdd33ae2
qweruiop <superinssa@gmail.com>
2021-07-15 14:12:27 +0900

    third commit

--- test_10.py
+++ test_10.py
@@ -2,5 +2,11 @@ import bs4
 
 import requests
 
+# 추가 입력됨. B : google.com, A: www.naver.com
+res = requests.get('www.google.com') # B
+
+if res.status_code == 200: # B
+    pass
+
 # 추가 입력됨.
 requests.get('www.naver.com')
