7f3a2f51ec46e5bf04a7eb34440c290c16110748
scw <kaya1436@daum.net>
2021-07-23 17:36:50 +0900

    commit

---
+++ scrapping/test_04.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+connect = sqlite3.connect('../db.sqlite3')
+
+# import pandas as pd
+
+df = pd.read_sql_query('select * from dbapp_economic de where id =', connect)
+
+# print(df.head(4))
